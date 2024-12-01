de6b59496d749e7aacebc70cc4b1872a48a51f2d
Robin Phillips <robinlmp@mac.com>
2024-12-01 16:18:57 +0000

    day 1 complete

--- Sources/AdventOfCode/Day 1/Day1.swift
+++ Sources/AdventOfCode/Day 1/Day1.swift
@@ -1,24 +1,72 @@
+import Algorithms
+
 struct Day1: Solution {
     static let day = 1
+    let pairs: Pairs
     
     /// Initialise your solution
     ///
     /// - parameters:
     ///   - input: Contents of the `Day1.input` file within the same folder as this source file
     init(input: String) {
-
+        self.pairs = Pairs(
+            pairs: input
+                .components(separatedBy: .newlines)
+                .map {
+                    $0.components(separatedBy: .whitespaces)
+                        .compactMap(Int.init)
+                    
+                }
+        )
     }
 
     /// Return your answer to the main activity of the advent calendar
     ///
     /// If you need to, you can change the return type of this method to any type that conforms to `CustomStringConvertible`, i.e. `String`, `Float`, etc.
     func calculatePartOne() -> Int {
-        0
+        pairs.totalDifference()
     }
 
     /// Return your solution to the extension activity
     /// _ N.B. This is only unlocked when you have completed part one! _
     func calculatePartTwo() -> Int {
-        0
+        pairs.part2Total()
+    }
+}
+
+extension Day1 {
+    struct Pairs {
+        let pairs: [[Int]]
+        
+        var leftList: [Int] {
+            pairs.compactMap(\.first).sorted()
+        }
+        var rightList: [Int] {
+            pairs.compactMap(\.last).sorted()
+        }
+        
+        func differenceAt(index: Int) -> Int {
+            abs(leftList[index] - rightList[index])
+        }
+        
+        func totalDifference() -> Int {
+            return leftList
+                .indices
+                .reduce(into: 0) { result, index in
+                    result += differenceAt(index: index)
+            }
+        }
+        
+        func numberMatchMultiply(number: Int) -> Int {
+            rightList.filter {
+                $0 == number
+            }.count * number
+        }
+        
+        func part2Total() -> Int {
+            leftList.reduce(into: 0) { result, number in
+                result += numberMatchMultiply(number: number)
+            }
+        }
     }
 }
--- Tests/AdventOfCodeTests/Day 1/Day1Tests.swift
+++ Tests/AdventOfCodeTests/Day 1/Day1Tests.swift
@@ -5,10 +5,35 @@ final class Day1Tests: XCTestCase, SolutionTest {
     typealias SUT = Day1
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        print(try sut.pairs)
+        try XCTAssertEqual(sut.calculatePartOne(), 11)
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.calculatePartTwo(), 0)
+        try XCTAssertEqual(sut.calculatePartTwo(), 31)
+    }
+}
+
+extension Day1Tests {
+    func testDifference() throws {
+        XCTAssertEqual(try sut.pairs.differenceAt(index: 0), 2)
+        XCTAssertEqual(try sut.pairs.differenceAt(index: 1), 1)
+        XCTAssertEqual(try sut.pairs.differenceAt(index: 2), 0)
+        XCTAssertEqual(try sut.pairs.differenceAt(index: 5), 5)
+        
+        let localSut = SUT.Pairs(
+            pairs: [[5, 1], [7, 7], [3, 3]]
+        )
+        XCTAssertEqual(localSut.differenceAt(index: 0), 2)
+        XCTAssertEqual(localSut.differenceAt(index: 1), 2)
+        XCTAssertEqual(localSut.differenceAt(index: 2), 0)
+    }
+    
+    func testNumberMatchMultiply() throws {
+        XCTAssertEqual(try sut.pairs.numberMatchMultiply(number: 3), 9)
+    }
+    
+    func testPart2Total() throws {
+        XCTAssertEqual(try sut.pairs.part2Total(), 31)
     }
 }
