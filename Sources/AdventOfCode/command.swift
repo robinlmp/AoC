2c5a1e0b3eea07afbec25b30dc6f7f8dcbe0b06b
Robin Phillips <robinlmp@mac.com>
2024-12-01 16:19:58 +0000

    added duration print out

--- Sources/AdventOfCode/command.swift
+++ Sources/AdventOfCode/command.swift
@@ -5,30 +5,30 @@ struct Runner {
     static func main() throws {
         try [
             Day1.self,
-            Day2.self,
-            Day3.self,
-            Day4.self,
-            Day5.self,
-            Day6.self,
-            Day7.self,
-            Day8.self,
-            Day9.self,
-            Day10.self,
-            Day11.self,
-            Day12.self,
-            Day13.self,
-            Day14.self,
-            Day15.self,
-            Day16.self,
-            Day17.self,
-            Day18.self,
-            Day19.self,
-            Day20.self,
-            Day21.self,
-            Day22.self,
-            Day23.self,
-            Day24.self,
-            Day25.self
+//            Day2.self,
+//            Day3.self,
+//            Day4.self,
+//            Day5.self,
+//            Day6.self,
+//            Day7.self,
+//            Day8.self,
+//            Day9.self,
+//            Day10.self,
+//            Day11.self,
+//            Day12.self,
+//            Day13.self,
+//            Day14.self,
+//            Day15.self,
+//            Day16.self,
+//            Day17.self,
+//            Day18.self,
+//            Day19.self,
+//            Day20.self,
+//            Day21.self,
+//            Day22.self,
+//            Day23.self,
+//            Day24.self,
+//            Day25.self
         ].forEach { day in
             try runDay(day)
         }
@@ -39,9 +39,24 @@ struct Runner {
         let solution = day.init(input: inputString)
         
         print("Day \(day.day)")
-        print("\tPart One: \(solution.calculatePartOne())")
-        print("\tPart Two: \(solution.calculatePartTwo())")
+        
+        run(note: "Part One",
+            calculate: solution.calculatePartOne)
+        run(note: "Part Two",
+            calculate: solution.calculatePartTwo)
         print("\n")
+    }
+    
+    static func run(
+        note: String,
+        calculate: () -> CustomStringConvertible
+    ) {
+        let start = Date()
+        print("\t\(note): ", calculate())
+        let end = Date()
+        let duration = end.timeIntervalSince(start)
+        
+        print("\tin: ", duration, " seconds")
     }
     
     private static func getInputString(filename: String) throws -> String {
