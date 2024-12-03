9cfe75531990c9186d2e85a30901a2014e8cabb4
Robin Phillips <robinlmp@mac.com>
2024-12-03 12:20:44 +0000

    day 2 part 1

--- Sources/AdventOfCode/Day 2/Day2.swift
+++ Sources/AdventOfCode/Day 2/Day2.swift
@@ -1,15 +1,58 @@
 struct Day2: Solution {
     static let day = 2
     
+    let input: [[Int]]
+    
     init(input: String) {
-
+        self.input = input
+            .components(separatedBy: .newlines)
+            .filter { !$0.isEmpty}
+            .map {
+                $0.components(separatedBy: .whitespaces)
+                    .compactMap(Int.init)
+            }
     }
 
     func calculatePartOne() -> Int {
-        0
+        input.filter {
+            isSafePart1(list: $0) == true
+        }.count
     }
     
     func calculatePartTwo() -> Int {
         0
+    }
+
+    func isSafePart1(list: [Int]) -> Bool {
+        let signs: [Int] = calculateSigns(
+            differences:
+                calculateDifferences(list: list)
+        )
+        
+        let set = Set(signs)
+        
+        if set.count == 1 {
+            return true
+        }
+        
+        return false
+    }
+    
+    func calculateDifferences(list: [Int]) -> [Int] {
+        return list.enumerated().compactMap {
+            if $0 > 0 {
+                $1 - list[$0 - 1]
+            } else { nil }
+        }
+    }
+    
+    func calculateSigns(differences: [Int]) -> [Int] {
+        return differences.map {
+            if abs($0) > 3 {
+                $0
+            } else {
+                $0.signum()
+            }
+        }
     }
 }
--- Tests/AdventOfCodeTests/Day 2/Day2Tests.swift
+++ Tests/AdventOfCodeTests/Day 2/Day2Tests.swift
@@ -5,10 +5,39 @@ final class Day2Tests: XCTestCase, SolutionTest {
     typealias SUT = Day2
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 2)
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.calculatePartTwo(), 0)
+        try XCTAssertEqual(sut.calculatePartTwo(), 4)
+    }
+}
+
+extension Day2Tests {
+    func testIsSafePart1() throws {
+        XCTAssertTrue(try sut.isSafePart1(list: [1,2,3,4,5]))
+        XCTAssertTrue(try sut.isSafePart1(list: [5,4,3,2,1]))
+        
+        XCTAssertFalse(try sut.isSafePart1(list: [5,4,8,2,1]))
+        XCTAssertFalse(try sut.isSafePart1(list: [1,2,4,3,1]))
+        
+        XCTAssertFalse(try sut.isSafePart1(list: [1,2,7,8,9]))
+        XCTAssertFalse(try sut.isSafePart1(list: [1,2,7,8,9].reversed()))
+        
+        XCTAssertTrue(try sut.isSafePart1(list: [1,2,5,6,8]))
+        XCTAssertFalse(try sut.isSafePart1(list: [1,2,5,5,4]))
+    }
+    
+    func testIsSafePart2() throws {
+        XCTAssertTrue(try sut.isSafePart2(list: [7,6,4,2,1]))
+        
+        XCTAssertFalse(try sut.isSafePart2(list: [1,2,7,8,9]))
+        XCTAssertFalse(try sut.isSafePart2(list: [9,7,6,2,1]))
+        
+        XCTAssertTrue(try sut.isSafePart2(list: [1,3,2,4,5]))
+        XCTAssertTrue(try sut.isSafePart2(list: [8,6,4,4,1]))
+        XCTAssertTrue(try sut.isSafePart2(list: [1,3,6,7,9]))
+        
+        XCTAssertFalse(try sut.isSafePart2(list: [66, 68, 69, 71, 72, 71, 72, 69]))
     }
 }
